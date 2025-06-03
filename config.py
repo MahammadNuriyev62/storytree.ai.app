700e15165ac7a5915a4d696932085751e6bd3ec0
MahammadNuriyev62 <maganuriyev@gmail.com>
2025-06-03 16:38:19 +0200

    don't require openai_api_key by default

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+from typing import Optional
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    openai_api_key: str = Field()
+    openai_api_key: Optional[str] = Field(default=None)
 
     class Config:
         env_file = ".env"
