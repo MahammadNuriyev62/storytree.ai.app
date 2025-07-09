246620cde3df0600bc9dcd3424c546a798960b4d
MahammadNuriyev62 <maganuriyev@gmail.com>
2025-07-09 12:24:49 +0400

    use env from the runtime

--- config.py
+++ config.py
@@ -6,8 +6,5 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     openai_api_key: Optional[str] = Field(default=None)
 
-    class Config:
-        env_file = ".env"
-
 
 settings = Settings()  # type: ignore
